90fbbe773a7a35b40d21f81eaa8b0aeb3f43c666
Pavel549 <sharapov.pavel2012@yandex.ru>
2021-03-11 22:12:41 +0300

    Create arrya, two loops.js
    Create an array with two loops:
    [ [ 'x', 'x', 'x', 'x' ], [ 'x', 'x', 'x', 'x' ], [ 'x', 'x', 'x', 'x' ] ]

---
+++ arrya, two loops.js
@@ -0,0 +1,13 @@
+/* Create an array with two loops:
+[ [ 'x', 'x', 'x', 'x' ], [ 'x', 'x', 'x', 'x' ], [ 'x', 'x', 'x', 'x' ] ] */
+
+let arr = [];
+
+for(let i = 0; i < 3; i++){
+    arr[i] = [];
+    for(let j = 0; j < 4; j++){
+        arr[i].push('x');
+    }
+}
+
+console.log(arr); 
