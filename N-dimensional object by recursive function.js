b685db8b5fea18344cda71a1f1a8efa866c9ff5d
Pavel549 <sharapov.pavel2012@yandex.ru>
2021-03-15 18:44:03 +0300

    Update N-dimensional object by recursive function.js
    There is a N-dimensional object. Use recursive function to display all
    the primitive elements of this object on the screen:
    {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7,
    m: {n: 8, o: 9}}}}

--- N-dimensional object by recursive function.js
+++ N-dimensional object by recursive function.js
@@ -9,7 +9,6 @@ let obj1 = {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7,
 
 function func(obj) {
 	for (let key in obj) {
-		//console.log(obj[key]);
 			if (typeof obj[key] == 'object') {
 				func(obj[key]);
 			} else {
