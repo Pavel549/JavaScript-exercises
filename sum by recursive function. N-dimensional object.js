c11607dbe758d76cd598643926be12c376a4d643
Pavel549 <sharapov.pavel2012@yandex.ru>
2021-03-15 15:18:28 +0300

    Update sum by recursive function. N-dimensional object.js
    There is a N-dimensional object. Count the sum of the elements of this object: {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}}

--- sum by recursive function. N-dimensional object.js
+++ sum by recursive function. N-dimensional object.js
@@ -1,3 +1,7 @@
+/* There is a N-dimensional object. Count the sum of the elements
+of this object: {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, 
+	m: {n: 8, o: 9}}}} */
+
 let obj = {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}}; 
 
 function func(obj){ 
